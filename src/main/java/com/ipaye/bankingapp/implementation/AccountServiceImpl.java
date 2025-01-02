10f57152e2515b70e7a9b51fe22586fc381091f1
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-01-02 23:52:09 +0100

    init commit

--- src/main/java/com/ipaye/bankingapp/implementation/AccountServiceImpl.java
+++ src/main/java/com/ipaye/bankingapp/implementation/AccountServiceImpl.java
@@ -8,6 +8,9 @@ import com.ipaye.bankingapp.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 public class AccountServiceImpl implements AccountService {
@@ -59,6 +62,13 @@ public class AccountServiceImpl implements AccountService {
         Account savedAccount = accountRepository.save(account);
         return AccountMapper.mapToAccountDto(savedAccount);
 
+    }
+
+    @Override
+    public List<AccountDto> getAllAccounts() {
+      List<Account> accounts = accountRepository.findAll();
+      return accounts.stream().map(account -> AccountMapper.mapToAccountDto(account))
+                .collect(Collectors.toList());
     }
 
 
--- src/main/java/com/ipaye/bankingapp/service/AccountService.java
+++ src/main/java/com/ipaye/bankingapp/service/AccountService.java
@@ -3,6 +3,8 @@ package com.ipaye.bankingapp.service;
 import com.ipaye.bankingapp.dto.AccountDto;
 import com.ipaye.bankingapp.entity.Account;
 
+import java.util.List;
+
 public interface AccountService {
 
     AccountDto createAccount(AccountDto accountDto);
@@ -12,4 +14,6 @@ public interface AccountService {
     AccountDto deposit(Long id, double amount);
 
     AccountDto withdraw(Long id, double amount);
+
+    List<AccountDto> getAllAccounts();
 }
